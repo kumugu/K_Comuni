7eff13fcc42badb3bd6d8f84f37b9db7f2b55411
kumugu <rmarn018@gmail.com>
2025-02-05 15:12:16 +0900

    create be fe

---
+++ comuni_be/comuni/src/main/java/com/comuni/comuni/ComuniApplication.java
@@ -0,0 +1,13 @@
+package com.comuni.comuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComuniApplication.class, args);
+	}
+
+}
---
+++ comuni_be/comuni/src/test/java/com/comuni/comuni/ComuniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comuni.comuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
